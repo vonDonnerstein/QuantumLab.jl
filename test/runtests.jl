c0ca45c742eb2a6a09950a6509e6eabf95b7e349
vonDonnerstein <daschemiker@googlemail.com>
2015-10-02 16:18:08 +0200

    readxyz and basic concepts of geometry

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,8 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+# test readxyz
+@test readxyz("h2o.xyz").atoms[2].element.symbol == "O"
+@test -0.752 < readxyz("h2o.xyz").atoms[3].position.x < -0.750
+
