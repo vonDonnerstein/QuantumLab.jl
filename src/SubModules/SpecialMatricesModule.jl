6b1dbacd83273eb65f9e066f6a759a33746c5d96
vonDonnerstein <daschemiker@googlemail.com>
2016-07-05 16:12:06 +0200

    decluttering RI

--- src/SubModules/SpecialMatricesModule.jl
+++ src/SubModules/SpecialMatricesModule.jl
@@ -1,8 +1,8 @@
 module SpecialMatricesModule
-export computeMatrixKinetic, computeTensorElectronRepulsionIntegrals, computeMatrixNuclearAttraction, computeMatrixOverlap, computeMatrixCoulomb, computeMatrixExchange, computeMatrixFock, computeMatricesResolutionOfTheIdentityCoulombMetric, computeMatricesResolutionOfTheIdentityOverlapMetric
+export computeMatrixKinetic, computeTensorElectronRepulsionIntegrals, computeMatrixNuclearAttraction, computeMatrixOverlap, computeMatrixCoulomb, computeMatrixExchange, computeMatrixFock
+using ..BaseModule
 using ..IntegralsModule
 using ..BasisModule
-using ..BaseModule
 using ..Geometry
 using TensorOperations
 using ..LibInt2Module
@@ -21,42 +21,6 @@ end
 
 function computeMatrixNuclearAttraction(basis::GaussianBasis,geo::Geometry)
   return [sum([NuclearAttractionIntegral(cgb1,cgb2,atom) for atom in geo.atoms]) for cgb1 in basis.contractedBFs, cgb2 in basis.contractedBFs]
-end
-
-function computeMatricesResolutionOfTheIdentityOverlapMetric(
-  basis::GaussianBasis,
-  basis_aux::GaussianBasis)
-  # compute B=(μν|P) and Ci=(PQ)^-1 ,Di=(Q|R) such that B Ci D Ci^T B^T ≈ (μν|λσ) (=ERI)
-  #
-  # example usage:
-  # B,Ci,D = computeMatricesResolutionOfTheIdentitOverlapMetric(bas,bas)
-  # using TensorOperations
-  # @tensor ERI[μ,ν,λ,σ] := B[μ,ν,P] * Ci[P,Q] * D[Q,R] * Ci[R,S] * B[λ,σ,S]
-  B = [computeValueThreeCenterOverlap(μ,ν,P) for μ in basis.contractedBFs, ν in basis.contractedBFs, P in basis_aux.contractedBFs] #B=(μν|P)
-  C = [computeValueOverlap(cgb1,cgb2) for cgb1 in basis_aux.contractedBFs, cgb2 in basis_aux.contractedBFs] #Ci=(PQ)^-1
-  D = [computeIntegral2Center2Electron(P,Q)   for P in basis_aux.contractedBFs, Q in basis_aux.contractedBFs] #D=(P|Q)
-	Ci = inv(C)
-	DiffCi = Ci - Ci'
-	MaxErrorCi = max(abs(DiffCi)...)
-	#FOR DEBUG PURRPOSES ONLY
-	#= println("Max Element inverse Overlap:  $(max(abs(Ci)...))") =#
-	#= println("Max Error inverse Overlap:    $(MaxErrorCi)") =#
-	#END OF DEBUGGING BLOCK
-  return B, inv(C), D, MaxErrorCi
-end
-
-function computeMatricesResolutionOfTheIdentityCoulombMetric(
-  basis::GaussianBasis,
-  basis_aux::GaussianBasis)
-  # compute B=(μν|P) and Ci=(P|Q)^-1, such that B Ci B^T ≈ (μν|λσ) (=ERI)
-  #
-  # example usage:
-  # B,Ci = computeMatricesResolutionOfTheIdentityCoulombMetric(bas,bas)
-  # using TensorOperations
-  # @tensor ERI[μ,ν,λ,σ] := B[μ,ν,P] * Ci[P,Q] * B[λ,σ,Q]
-  B = [computeIntegral3Center2Electron(μ,ν,P) for μ in basis.contractedBFs,          ν in basis.contractedBFs,          P in basis_aux.contractedBFs]
-  C = [computeIntegral2Center2Electron(P,Q)   for P in basis_aux.contractedBFs, Q in basis_aux.contractedBFs]
-  return B, inv(C)
 end
 
 function computeMatrixCoulomb(basis::GaussianBasis, density::Matrix)
