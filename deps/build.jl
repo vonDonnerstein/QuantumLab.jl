9f45a6c6f2a5fdb0fc2938eb4d7ca13053fc9db6
Joern Dietze <j.dietze@posteo.de>
2016-07-18 14:22:34 +0200

    Some changes in build.jl related to linking libraries

--- deps/build.jl
+++ deps/build.jl
@@ -1,70 +1,15 @@
-using BinDeps
+LIBINT_VERSION = "2.2.0-beta1"
+lib_path = joinpath(Pkg.dir("QuantumLab"),"deps","usr","lib")
 
-@BinDeps.setup
+@linux_only begin
+	cd(joinpath(Pkg.dir("QuantumLab"),"deps","usr","lib"))
+	run(`ln -s libint2jl-$LIBINT_VERSION.so libint2jl.so`)
+	run(`ln -s libint2-$LIBINT_VERSION.so libint2-QuantumLab.so`)
+end
 
-libint2jl = library_dependency("libint2jl-QuantumLab.so")
-libint2   = library_dependency("libint2-QuantumLab.so")
+@osx_only begin
+	cd(joinpath(Pkg.dir("QuantumLab"),"deps","usr","lib"))
+	run(`ln -s libint2jl-$LIBINT_VERSION.dylib libint2jl.so`)
+	run(`ln -s libint2-$LIBINT_VERSION.dylib libint2-QuantumLab.so`)
+end
 
-prefix = joinpath(BinDeps.depsdir(libint2jl))
-lib_dir = BinDeps.libdir(libint2jl)
-VERSION = "2.2.0-beta1"
-#= download_dir = joinpath(prefix,"downloads") =#
-#= provides(SimpleBuild, =#
-#= 				(@build_steps begin =#
-#= 					CreateDirectory(download_dir) =#
-#= 					(@build_steps begin =#
-#= 						ChangeDirectory(download_dir) =#
-#= 						FileRule(joinpath(prefix,"usr","lib","libint2.so"), @build_steps begin =#
-#= 							`wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libi/libint2/libint2-dev_2.1.0~beta2-2_amd64.deb` =#
-#= 							`ar -vx libint2-dev_2.1.0~beta2-2_amd64.deb` =#
-#= 							`tar -xvzf data.tar.gz -C $prefix` =#
-#= 							`rm -f debian-binary control.tar.gz data.tar.xz` =#
-#= 						end) =#
-#= 					end) =#
-#= 				end), libint2, os = :Linux) =#
-
-#Sets symbolic link for precompiled libint2.so and libint2jl.so in deps/bin directory for Linux
-provides(SimpleBuild,
-				 (@build_steps begin
-					CreateDirectory(lib_dir)
-					@build_steps begin
-						ChangeDirectory(lib_dir)
-						FileRule(joinpath(prefix,"usr","lib","libint2jl-QuantumLab.so"),
-								@build_steps begin
-									`ln -s libint2jl-$VERSION.so libint2jl-QuantumLab.so`
-								end)
-						end
-		 end), libint2jl, os = :Linux)
-provides(SimpleBuild,
-				(@build_steps begin
-					CreateDirectory(lib_dir)
-					ChangeDirectory(lib_dir)
-					FileRule(joinpath(prefix,"usr","lib","libint2-QuantumLab.so"),
-					@build_steps begin
-						`ln -s libint2-$VERSION.so libint2-QuantumLab.so`
-					end)
-				end), libint2, os = :Linux)
-
-#Sets symbolic link for precompiled libint2.so and libint2jl.so in deps/bin directory for OSX (Darwin)
-provides(SimpleBuild,
-				(@build_steps begin
-					CreateDirectory(lib_dir)
-					ChangeDirectory(lib_dir)
-					FileRule(joinpath(prefix,"usr","lib","libint2jl-QuantumLab.so"),
-					@build_steps begin
-						`ln -s libint2jl-$VERSION.dylib libint2jl-QuantumLab.so`
-					end)
-				end), libint2jl, os = :Darwin)
-provides(SimpleBuild,
-				(@build_steps begin
-					CreateDirectory(lib_dir)
-					ChangeDirectory(lib_dir)
-					FileRule(joinpath(prefix,"usr","lib","libint2-QuantumLab.so"),
-					@build_steps begin
-						`ln -s libint2-$VERSION.dylib libint2-QuantumLab.so`
-					end)
-				end), libint2, os = :Darwin)
-
-
-@BinDeps.install Dict(:libint2jl => :libint2jl,
-							:libint2 => :libint2)
