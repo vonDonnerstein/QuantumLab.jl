1b84e7b5c3c2306af879824e02bb49c1ab1954c9
Joern Dietze <j.dietze@posteo.de>
2016-08-04 19:11:30 +0200

    Extended build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -3,28 +3,44 @@ lib_path = joinpath(Pkg.dir("QuantumLab"),"deps","usr","lib")
 
 
 @linux_only begin
-	try run(pipeline(`grep --ignore-case 'avx\|sse' /proc/cpuinfo`))
-	catch ; error("Your CPU does support neither SSE nor AVX")
+	try begin 
+			run(`grep --ignore-case --silent 'avx' /proc/cpuinfo`)
+			SIMD = "avx"
+			cd(joinpath(Pkg.dir("QuantumLab"),"deps","usr","lib"))
+			run(`ln -s libint2jl-$LIBINT_VERSION-$SIMD.so libint2jl.so`)
+			run(`ln -s libint2-$LIBINT_VERSION-$SIMD.so libint2-QuantumLab.so`)
+		end
+	catch
+		warn("Your system (CPU) does not support AVX.\n
+							The Libint2 Library for Linux is only provided as AVX enabled version.\n
+							Therefore a Libint2 stub is used.\n
+							You should be able to start QuantumLab and perform calculation but
+							with a drastically decreased performance.\n
+							You can try to build your own version of libint2.so and libint2jl.so.
+							As a reference take a look at the Makefile in QuantumLab/deps/usr/src.")
 	end
-	SIMD = "avx"
-	try run(`grep 'avx' --ignore-case /proc/cpuinfo`)
-	catch ; SIMD = "sse"
-	end
-	cd(joinpath(Pkg.dir("QuantumLab"),"deps","usr","lib"))
-	run(`ln -s libint2jl-$LIBINT_VERSION-$SIMD.so libint2jl.so`)
-	run(`ln -s libint2-$LIBINT_VERSION-$SIMD.so libint2-QuantumLab.so`)
 end
 
 @osx_only begin
-	try run(pipeline(`sysctl machdep.cpu.features`,`grep --ignore-case 'avx\|sse'`))
-	catch ; error("Your CPU does support neither SSE nor AVX")
+	try begin
+			run(pipeline(`sysctl machdep.cpu.features`,`grep --ignore-case  --silent 'avx\|sse'`))
+			SIMD = "avx"
+			try run(pipeline(`sysctl machdep.cpu.features`,`grep --ignore-case --silent 'avx'`))
+			catch 
+				SIMD = "sse"
+			end
+			cd(joinpath(Pkg.dir("QuantumLab"),"deps","usr","lib"))
+			run(`ln -s libint2jl-$LIBINT_VERSION-$SIMD.dylib libint2jl.so`)
+			run(`ln -s libint2-$LIBINT_VERSION-$SIMD.dylib libint2-QuantumLab.so`)
+		end
+	catch
+		warn("Your CPU does support neither SSE nor AVX
+							The Libint2 Library for Linux is only provided as AVX enabled version.\n
+							Therefore a Libint2 stub is used.\n
+							You should be able to start QuantumLab and perform calculation but
+							with a drastically decreased performance.\n
+							You can try to build your own version of libint2.dylib and libint2jl.dylib.
+							As a reference take a look at the Makefile in QuantumLab/deps/usr/src.")
 	end
-	SIMD = "avx"
-	try run(pipeline(`sysctl machdep.cpu.features`,`grep --ignore-case 'avx'`))
-	catch ; SIMD = "sse"
-	end
-	cd(joinpath(Pkg.dir("QuantumLab"),"deps","usr","lib"))
-	run(`ln -s libint2jl-$LIBINT_VERSION-$SIMD.dylib libint2jl.so`)
-	run(`ln -s libint2-$LIBINT_VERSION-$SIMD.dylib libint2-QuantumLab.so`)
 end
 
